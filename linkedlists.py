7e3814217e91fa502f524abbd8126054fbacd1b1
chetat <yekuwilfred@gmail.com>
2020-06-26 23:52:34 +0100

    Implement insert at beginning and printlist methods

--- linkedlists.py
+++ linkedlists.py
@@ -1,15 +1,33 @@
+# A simple node
 class Node:
+    # Create initialize node
     def __init__(self, value, next_node):
         self.next = next_node
         self.data = value
-   
+    
+    def get_data(self):
+        return str(self.data)
+    
+    def set_data(self, value):
+        self.data = value
+    
+
+
 
 class LinkedList:
-    def __init__(self, head, node):
-        pass
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.count = 0
+    
+    # Insert Node at the begining of linkedlist
+    def insert(self, data):
+        new_node = Node(data, self.head)
+        self.head = new_node
+        self.count += 1
 
-    def insert(self):
-        pass
+        if self.count == 1:
+            self.tail = self.head
 
     def insert_mid(self):
         pass
@@ -18,24 +36,34 @@ class LinkedList:
         pass
 
     def delete(self):
-        pass 
+        pass
 
+    def get_size(self):
+        return f"{self.count} Items"
 
+    def find(self):
+        pass
 
-first_node = Node(5, None)
+    def print_list(self):
+        while(self.head != None):
+            print(self.head.data)
+            self.head = self.head.next
+        if (self.head == None):
+            print("Emptied linkedlist")
 
-midle_node = Node(8, None)
+first_node = 45
+
+midle_node = 5
+
+third_node = {"name": "wilfred"}
 
 last_node = Node(56, None)
-
-first_node.next = midle_node
-
-midle_node.next = last_node
-
-def print_nodes(node):
-    while(node != None):
-        print(node.data)
-        node = node.next
+linkedlist = LinkedList()
+linkedlist.insert(first_node)
+linkedlist.insert(midle_node)
+linkedlist.insert(third_node)
 
 
-print_nodes(first_node)
+
+print(linkedlist.get_size())
+linkedlist.print_list()
